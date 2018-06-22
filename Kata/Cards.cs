443ff92e4da409def1e5f3e22cb8da1a43745c9b
Kent <utterback.kent.a@gmail.com>
2018-06-22 09:27:32 -0400

    Create deck of cards object

---
+++ Kata/Cards.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata
+{
+    public class Cards
+    {
+        public readonly int[] Deck = { 0, 0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8 };
+    }
+}
